c71199875c865a85f981d40b3f91a89e31988680
Marina <mljuboja@luc.edu>
2024-04-06 16:07:10 -0500

    Update #3

--- src/main/java/com/group2/project/ProjectApplication.java
+++ src/main/java/com/group2/project/ProjectApplication.java
@@ -9,6 +9,8 @@ public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 		//Koji was here
+
+		//comment made in Marina's Branch
 	}
 
 }
