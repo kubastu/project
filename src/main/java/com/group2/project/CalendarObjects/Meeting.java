1a26d2211fee92237c6a6c82c85c8eae2d3998d3
Zywicki <kubastudent2003@gmail.com>
2024-05-04 14:49:20 -0500

    calendar object(s) + sql annotations + lombok

---
+++ src/main/java/com/group2/project/CalendarObjects/Meeting.java
@@ -0,0 +1,23 @@
+package com.group2.project.CalendarObjects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Getter
+public class Meeting extends CalendarObject
+{
+
+    @Column(name = "LOCATION")
+    private String location;
+
+    public Meeting(Date date, String title, String description, String location) {
+        super(date, title, description);
+        this.location = location;
+    }
+}
--- src/main/java/com/group2/project/ProjectApplication.java
+++ src/main/java/com/group2/project/ProjectApplication.java
@@ -1,23 +1,19 @@
 package com.group2.project;
 
+import com.group2.project.CalendarObjects.CalendarObject;
+import com.group2.project.CalendarObjects.Event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
-		randomFunction();
-
-	}
-	public static void randomFunction(){
-		System.out.println("Hello");
-		// adding comment here
-	}
-
-	public static void anotherFunction(){
-		System.out.println("another function");
+		CalendarObject test = new Event(new Date(), "my event", "this is my description", "11:00am");
+		System.out.println(test.toString());
 	}
 
 }
