3371a519c868afa4518777d2c0e147a1d480ae14
kubastu <kubastudent2003@gmail.com>
2024-05-14 21:10:09 -0500

    Added REPL for interacting with CalendarManager

--- src/main/java/com/group2/project/ProjectApplication.java
+++ src/main/java/com/group2/project/ProjectApplication.java
@@ -5,15 +5,58 @@ import com.group2.project.calendarobjects.Event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ProjectApplication {
 
+	private static boolean shouldRun = true;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
-		CalendarObject test = new Event(new Date(), "my event", "this is my description", "11:00am");
-		System.out.println(test.toString());
+
+		runREPL();
+	}
+
+	// JZ: this function defines a simple Read-Eval-Print-Loop for a basic calendar I/O.
+	private static void runREPL() {
+
+		// Insight from : https://www.tutorialspoint.com/how-can-we-read-from-standard-input-in-java
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.print("REPL Options : \n (A)dd \n (R)emove \n (L)ist [All] \n (E)xit \n");
+			String option = br.readLine();
+
+			while(!option.equals("E")) {
+				if(option.equals("A")) {
+					System.out.println("Title ?");
+					String tempTitle = br.readLine();
+					System.out.println("Description ?");
+					String tempDescription = br.readLine();
+
+					CalendarObject temp = new CalendarObject(new Date(), tempTitle, tempDescription);
+					CalendarManager.addCalendarObject(temp);
+				} else if(option.equals("R")) {
+					// TODO: Implement date + id for this to work
+				} else if(option.equals("L")) {
+					System.out.println(CalendarManager.listCalendarObjects());
+				}
+
+				System.out.println("#########################");
+				System.out.print("REPL Options : \n (A)dd \n (R)emove \n (L)ist [All] \n (E)xit \n");
+				option = br.readLine();
+
+			}
+
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
