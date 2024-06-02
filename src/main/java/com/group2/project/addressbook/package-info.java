746bc3fd65c7af43dac664b5441a341354397be8
Marina <mljuboja@luc.edu>
2024-06-02 13:26:50 -0500

    Update

--- src/main/java/com/group2/project/addressbook/package-info.java
+++ src/main/java/com/group2/project/addressbook/package-info.java
@@ -1,1 +1,13 @@
+/**
+ * <b>Demonstrate my knowledge I have acquired from the class
+ * <ul>
+ * <li>Unit Tests
+ * <li>Documentations on features that were not covered in class
+ * <li>Repository with additional finders
+ * <li>Added unit tests to demonstrate repository working
+ * <li>Added additional notes on services for swagger and services unit test using MockMVC
+ * <li>Combined SQL with NoSQL
+ * </ul>
+ * @author Marina Ljuboja
+ */
 package com.group2.project.addressbook;
