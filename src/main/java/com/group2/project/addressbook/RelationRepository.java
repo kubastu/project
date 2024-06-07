2c47ea31bdbfc882406b0dc95228ba53e8df9343
Marina <mljuboja@luc.edu>
2024-06-07 15:26:09 -0500

    update

--- src/main/java/com/group2/project/addressbook/RelationRepository.java
+++ src/main/java/com/group2/project/addressbook/RelationRepository.java
@@ -2,6 +2,8 @@ package com.group2.project.addressbook;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RelationRepository extends MongoRepository<Relation, String>{
-    //public Relation findById(id);
+import java.util.List;
+
+public interface RelationRepository extends MongoRepository<Relation.RelationshipType, String>{
+    //List<Relation.RelationshipType> findByRelationshipType(Relation.relationshipType);
 }
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into contacts(first_name, last_name) values ('Susan', 'Cooper');
-insert into contacts(first_name, last_name) values ('John', 'Smith');
