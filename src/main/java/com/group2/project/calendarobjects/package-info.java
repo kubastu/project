87fae042fcfc6d09f1e56cdf58697d631e1005f1
Zywicki <kubastudent2003@gmail.com>
2024-06-06 20:25:13 -0500

    added descriptions in package-info.java

--- src/main/java/com/group2/project/calendarobjects/package-info.java
+++ src/main/java/com/group2/project/calendarobjects/package-info.java
@@ -1,1 +1,11 @@
 package com.group2.project.calendarobjects;
+
+/**
+ * <b>This package defines Java object classes for our representations of calendar events.
+ * <ul>
+ * <li> CalendarObject: Defines the base class for what we expect an 'event' to hold, that is: A date, a title, and a description.
+ * <li> Event: Defines a class that inherits from CalendarObject, but also contains a time.
+ * <li> Meeting: Defines a class that inherits from CalendarObject, but also contains a location.
+ * </ul>
+ * @author Jakub Zywicki, Marina Ljuboja, Benito Ohara
+ */
