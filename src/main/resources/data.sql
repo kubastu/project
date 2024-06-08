25cd798376ac559367b55fbb87fbd9af73028f43
Marina <mljuboja@luc.edu>
2024-06-07 20:06:49 -0500

    update from ML

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+create table contacts (first_name varchar(25), last_name varchar(25));
+
+insert into contacts(first_name, last_name) values ('Jane', 'Doe');
+insert into contacts(first_name, last_name) values ('John', 'Smith');
