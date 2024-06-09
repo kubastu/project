ff19eff2059e853e9c959d756a177531c1258396
Koji Ohara <pacutacatete100@gmail.com>
2024-06-09 02:59:22 -0500

    package-info.java update

--- src/main/java/com/group2/project/Weather/package-info.java
+++ src/main/java/com/group2/project/Weather/package-info.java
@@ -1,1 +1,14 @@
+/**
+ * This package contains classes for retrieving and displaying weather data
+ * for a specific location (Chicago).
+ * <p>
+ * The main components of this package include:
+ * </p>
+ * <ul>
+ *   <li>{Weather}: A class that handles the retrieval of weather data from an external API using the specified latitude and longitude.</li>
+ *   <li>{WeatherData}: A data class that encapsulates weather information such as temperature, maximum and minimum temperatures, sunrise, and sunset times.</li>
+ *   <li>{Main}: The entry point of the application which interacts with the {Weather} class to fetch and display weather data.</li>
+ * </ul>
+ * @author Benito Ohara
+ **/
 package com.group2.project.Weather;
