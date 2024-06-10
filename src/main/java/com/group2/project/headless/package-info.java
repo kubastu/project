6b462e90189e90d84756f06b46c2bbd357c2c1ef
kubastu <166253549+kubastu@users.noreply.github.com>
2024-06-10 01:56:38 -0500

    more commenting in package-info's

--- src/main/java/com/group2/project/headless/package-info.java
+++ src/main/java/com/group2/project/headless/package-info.java
@@ -1,1 +1,9 @@
 package com.group2.project.headless;
+/**
+ * <b> This package defines Java Headless code designed to run and test CRUD operation on CalendarObjects (for an earlier version of our project).
+ * <ul>
+ * <li> {REPL} defines a static Java class that evaluates user-read input in the form of a Read-Evaluate-Print-Loop, and Creates, Updates, Reads, and Deletes CalendarObjects as desired.
+ * <li> {CalendarManager} defines a singleton manager that stores instances of CalendarObjects for usage in REPL class.
+ * </ul>
+ * @author Jakub Zywicki
+ */
