b6a94ab3cafe4315128d8a71d3cdd0eedbb01369
kubastu <166253549+kubastu@users.noreply.github.com>
2024-06-10 01:51:40 -0500

    began simple commenting + clean up

--- src/main/java/com/group2/project/email/Email.java
+++ src/main/java/com/group2/project/email/Email.java
@@ -4,6 +4,7 @@ package com.group2.project.email;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 
 
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Email implements EmailService{
     @Autowired
-    private JavaMailSender emailSender;
+    private JavaMailSender emailSender = new JavaMailSenderImpl(); // todo maybe change this
 
     @Autowired
     public SimpleMailMessage template;
@@ -21,17 +22,23 @@ public class Email implements EmailService{
     sendSimpleMessage(to, subject, text);
      */
 
+    // for sending a simple email, sample executable code is defined below:
+//    Thread emailSender = new Thread(() -> {
+//        Email sender = new Email();
+//        sender.sendSimpleMessage("<RECIPIENT EMAIL HERE>", "Test", "Works!");
+//    });
+//    emailSender.start();
+
     @Override
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("noreply@baeldung.com");
+        //message.setFrom("noreply@baeldung.com");
+        message.setFrom("calendarspringapp@gmail.com");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
     }
-
-
 
 }
 
