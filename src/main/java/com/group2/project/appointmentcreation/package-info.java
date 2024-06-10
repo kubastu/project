f28e0f4de43935673909611fe8d02175db6c0e1d
Koji Ohara <pacutacatete100@gmail.com>
2024-06-10 18:50:17 -0500

    update package-info.java in Appointments

--- src/main/java/com/group2/project/appointmentcreation/package-info.java
+++ src/main/java/com/group2/project/appointmentcreation/package-info.java
@@ -1,1 +1,15 @@
+/**
+ * This package contains classes for Appointment Creation
+ * <p>
+ * The main components of this package include:
+ * </p>
+ * <ul>
+ *   <li>{Appointment}: A class that defines the Appointment object</li>
+ *   <li>{AppointmentController}: Class that defines the CRUD operations for Appointments
+ *   <li>{AppointmentRepo}: Interface for the repository of appointments</li>
+ *   <li>{AppointmentService}: A class for logging the actions of the CRUD operations</li>
+ * </ul>
+ * @author Benito Ohara
+ **/
+
 package com.group2.project.appointmentcreation;
